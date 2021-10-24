6e1fc905239e9945b66ac52b74597bf77fd41a81
B4TT3RY <gb4tt3ry@gmail.com>
2021-10-24 22:09:55 +0900

    Show svg in background

--- src/components/MartButton.tsx
+++ src/components/MartButton.tsx
@@ -8,10 +8,9 @@ interface Props {
 
 export default function MartButton({ type, title, subTitle }: Props) {
   return (
-    <div className={`${type} relative w-24 h-16 pt-2.5 pl-2 rounded-xl font-black`}>
+    <div className={`mart-button ${type} relative w-24 h-16 pt-2.5 pl-2 rounded-xl font-black`}>
         <h1 className="mart-button-title text-lg leading-none">{title}</h1>
         <h2 className="mart-button-subtitle -mt-1">{subTitle}</h2>
-        <embed className="mart-button-logo absolute bottom-0 right-0 rounded-br-xl" src={`/logos/${type}.svg`}/>
     </div>
   );
 }
