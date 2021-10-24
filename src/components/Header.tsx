fed63950a86bbb92091ca1a8c3792a3f1140d3a0
B4TT3RY <gb4tt3ry@gmail.com>
2021-10-25 01:28:48 +0900

    Fixed center p tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="flex rounded-br-3xl bg-white p-5">
         <h1 className="flex-auto text-2xl font-black">오늘 마트 여나요?</h1>
-        <p className="flex-auto text-right text-sm">
+        <p className="flex-auto text-right text-sm m-auto">
             현재 시각 오전 11:23 <WbSunny fontSize="inherit" />
         </p>
     </div>
