4ab57390f4ad758451ea43c76433990efc39576f
B4TT3RY <gb4tt3ry@gmail.com>
2021-10-25 00:20:50 +0900

    [WIP] Added Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 
 import MartButton from './components/MartButton'
 import VisitedMart from './components/VisitedMart'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gray-100 h-screen">
       <div className="flex">
         <MartButton type="emart" title="이마트" />
         <MartButton type="traders" title="트레이더스" subTitle="이마트" />
@@ -15,6 +16,7 @@ function App() {
       </div>
 
       <VisitedMart type="traders" name="스타필드 안성점" status="영업중"/>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { WbSunny } from "@mui/icons-material";
+
+export default function Header() {
+  return (
+    <div className="flex rounded-br-3xl bg-white p-5">
+        <h1 className="flex-auto text-2xl font-black">오늘 마트 여나요?</h1>
+        <p className="flex-auto text-right text-sm">
+            현재 시각 오전 11:23 <WbSunny fontSize="inherit" />
+        </p>
+    </div>
+  );
+}
